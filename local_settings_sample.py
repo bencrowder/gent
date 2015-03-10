f25f629c6448f5cf92b7b9ac44d93e45b44242b6
bencrowder <ben.crowder@gmail.com>
2015-03-10 09:55:26 -0600

    Updates local_settings_sample.py

--- local_settings_sample.py
+++ local_settings_sample.py
@@ -7,11 +7,4 @@ DEBUG = TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'data.db'),
-    }
-}
-
 TIME_ZONE = 'America/Denver'
