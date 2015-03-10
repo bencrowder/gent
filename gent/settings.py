bbb8f0da5657f8f86cd0d007af3d4de3bbe95179
bencrowder <ben.crowder@gmail.com>
2015-03-10 09:53:30 -0600

    Adds database back to settings

--- gent/settings.py
+++ gent/settings.py
@@ -40,6 +40,14 @@ ROOT_URLCONF = 'gent.urls'
 WSGI_APPLICATION = 'gent.wsgi.application'
 
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'data.db'),
+    }
+}
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
