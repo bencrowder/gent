f08e3e7cbbd34a03ef5673f35ae659d7f721ee58
bencrowder <ben.crowder@gmail.com>
2015-03-10 08:08:03 -0600

    Adds CSRF support to JavaScript

--- gent/static/app.js
+++ gent/static/app.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
+	// From https://gist.github.com/alanhamlett/6316427
+	$.ajaxSetup({
+		beforeSend: function(xhr, settings) {
+			if (settings.type == 'POST' || settings.type == 'PUT' || settings.type == 'DELETE' || settings.type == 'PATCH') {
+				xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+			}
+		}
+	});
+
+
 	$("ul.sortable").sortable({
 		placeholder: "placeholder container",
 		update: function(event, ui) {
@@ -399,3 +409,21 @@ $(document).ready(function() {
 		return false;
 	});
 });
+
+
+// From https://gist.github.com/alanhamlett/6316427
+function getCookie(name) {
+	var cookieValue = null;
+	if (document.cookie && document.cookie != '') {
+		var cookies = document.cookie.split(';');
+		for (var i=0; i<cookies.length; i++) {
+			var cookie = jQuery.trim(cookies[i]);
+			// Does this cookie string begin with the name we want?
+			if (cookie.substring(0, name.length + 1) == (name + '=')) {
+				cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+				break;
+			}
+		}
+	}
+	return cookieValue;
+}
